86665ed26abef6fcec3935c5811ebde9a819044a
Jonas Sourlier <jonas.sourlier@yolp.ch>
2025-05-10 09:54:15 +0200

    added regex bug test

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,77 +1,184 @@
-import { Image, StyleSheet, Platform, Pressable, Text, Alert } from 'react-native';
+import { Image, StyleSheet, Platform, Pressable, Text, Alert, View, useColorScheme } from 'react-native';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import { useCallback } from 'react';
+
+interface SearchResultItem {
+  text: string;
+  translation?: string;
+  topTag?: string;
+  bottomTag: string;
+}
+
+const searchResults: SearchResultItem[] = [{
+  text: 'Capisce?',
+  translation: 'Do you understand?',
+  bottomTag: 'not yet',
+}, {
+  text: 'capire',
+  translation: 'to understand',
+  bottomTag: 'not yet',
+}, {
+  text: 'dummy',
+  bottomTag: 'not yet',
+}, {
+  text: 'dummy',
+  bottomTag: 'not yet',
+}, {
+  text: 'dummy',
+  bottomTag: 'not yet',
+}, {
+  text: 'dummy',
+  bottomTag: 'not yet',
+}, {
+  text: 'dummy',
+  bottomTag: 'not yet',
+}, {
+  text: 'dummy',
+  bottomTag: 'not yet',
+}, {
+  text: 'dummy',
+  bottomTag: 'not yet',
+}]
 
 export default function HomeScreen() {
-  const onClick = () => {
-    Alert.alert(`Please use a valid mail address.`);
-  }
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-        <Pressable onPress={onClick}><Text>Click me!</Text></Pressable>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <ScrollView
+      style={[styles.resultsContainer]}
+      //animatedProps={animatedScrollViewContainerProps}
+      contentContainerStyle={styles.resultsContainerInner}
+      keyboardShouldPersistTaps="never"
+    >
+      {searchResults.map((item, index) => (
+        <QueryResultView item={item} key={item.text} />
+      ))}
+    </ScrollView>
+  );
+}
+
+const QueryResultView = ({ item }: { item: SearchResultItem }) => {
+  const now = Date.now();
+  const isDark = useColorScheme() === 'dark';
+  const onPress = useCallback(() => {
+    Alert.alert(item.text);
+  }, []);
+  return (
+    <TouchableOpacity
+      style={[styles.item]}
+      onPress={onPress}
+    >
+      <View style={styles.itemContainer}>
+        <View style={styles.topContainer}>
+          <Text style={[styles.itemTitle, styles.fg]}>
+            {item.text}
+            {item.translation && <Text style={[styles.fontWeight400, styles.dimmedFg]}>{` - ${item.translation}`}</Text>}
+          </Text>
+          <Text style={[styles.standardSmallScript, styles.dimmedFg, styles.rightText, styles.rightText]}>
+            {item.topTag}
+          </Text>
+        </View>
+        <View style={styles.bottomContainer}>
+          <Text style={[styles.itemSubtitle, styles.fg]}>
+            🇮🇹 Italian Words
+          </Text>
+          <Text style={[styles.standardSmallScript, styles.dimmedFg3, styles.rightText, styles.rightText]}>
+            {item.bottomTag}
+          </Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  resultsContainer: {
+    flex: 1,
+    //borderWidth: 1, borderColor: 'green',
+    backgroundColor: 'white',
+    marginTop: 100,
+    marginBottom: 100,
+  },
+  resultsContainerInner: {
+    padding: 0,
+    margin: 0,
+    paddingTop: 16,
+    paddingBottom: 45,
+    //borderWidth: 1, borderColor: 'pink',
+  },
+  item: {
+    overflow: 'hidden',
+    //height: 44,
+    paddingHorizontal: 16,
+    marginBottom: 4,
+    //borderTopWidth: 1,
+    //borderBottomWidth: 1,
+    //borderColor: 'transparent',
+    paddingTop: 4,
+    paddingBottom: 4,
+    //paddingHorizontal: 8,
+    borderRadius: 6,
+    justifyContent: 'space-between',
+  },
+  selectedItem: {
+    //borderColor: '#44AAFF',
+    //backgroundColor: 'rgba(60, 140, 255, 0.2)',
+  },
+  itemContainer: {
+    flex: 1,
+  },
+  topContainer: {
     flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+    //backgroundColor: 'red',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  bottomContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
+    //backgroundColor: 'red',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  itemTitle: {
+    fontWeight: '600',
+    fontSize: 16,
+    flex: 1,
+    marginBottom: 2,
+    //backgroundColor: 'red',
+    //borderWidth: .333,
+    //borderColor: 'red',
+  },
+  itemSubtitle: {
+    fontSize: 14,
+    flexWrap: 'nowrap',
+    maxHeight: 16,
+    //backgroundColor: 'red',
+  },
+  rightText: {
+    //flexWrap: 'wrap',
+    lineHeight: 19,
+    marginTop: 1,
+    marginBottom: -3,
+  },
+  fg: {
+    color: '#000',
+  },
+  dimmedFg: {
+    color: '#000',
+    opacity: 0.8,
+  },
+  dimmedFg3: {
+    color: '#000',
+    opacity: 0.3,
+  },
+  fontWeight400: {
+    fontWeight: '400',
+  },
+  standardSmallScript: {
+    fontSize: 12,
+    lineHeight: 16,
+    fontWeight: '400',
   },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -29,11 +30,13 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+      <GestureHandlerRootView>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </GestureHandlerRootView>
     </ThemeProvider>
   );
 }
